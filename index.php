a2e2e1b5923879bb3bc7b8ad94effa3b72966983
pakpoom <2549pakpoom@gmail.com>
2024-08-07 15:32:18 +0700

    remove row

--- index.php
+++ index.php
@@ -209,19 +209,16 @@ input:checked + .slide-container .nav label { display: block; }
             </ul>
         </div>
         <div class="d-flex justify-content-center align-items-center">
-    <iframe width="560" height="315" 
-        src="https://www.youtube.com/embed/w9-TRoEa0jk?autoplay=1&mute=1" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" 
-        allowfullscreen>
-    </iframe>
-</div>
-
-
+            <iframe width="560" height="315" 
+                src="https://www.youtube.com/embed/w9-TRoEa0jk?autoplay=1&mute=1" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe>
+        </div>
     </div>
-    
 
     <?php include './footer.php'; ?>
 </body>
